6ce75b32007727225ceb481810f132763c780009
Santiago Narvaez <santiagonar1@gmail.com>
2025-11-18 13:18:59 +0100

    format: apply clang format to file

--- main.cpp
+++ main.cpp
@@ -4,37 +4,33 @@
 
 int main() {
     auto simple_arena = SimpleArena<1024>{};
-    std::cout << " ======== Simple Arena ========\n\n";
+    std::cout << "\n\n ======== Simple Arena ========\n\n";
 
     std::cout << "allocated buffer at " << simple_arena._buffer << " ("
               << sizeof simple_arena._buffer << " bytes)\n";
 
     // Allocate chars
-    if (auto* p = simple_arena.create<char>(1))
-    {
+    if (auto *p = simple_arena.create<char>(1)) {
         *p = 'a';
-        std::cout << "allocated a char at " << (void*)p << '\n';
+        std::cout << "allocated a char at " << (void *) p << '\n';
     }
 
     // Allocate one int
-    if (auto* p = simple_arena.create<int>(1))
-    {
+    if (auto *p = simple_arena.create<int>(1)) {
         *p = 1;
-        std::cout << "allocated an int at " << (void*)p << '\n';
+        std::cout << "allocated an int at " << (void *) p << '\n';
     }
 
     // Allocate chars
-    if (auto* p = simple_arena.create<char>(1))
-    {
+    if (auto *p = simple_arena.create<char>(1)) {
         *p = 'a';
-        std::cout << "allocated a char at " << (void*)p << '\n';
+        std::cout << "allocated a char at " << (void *) p << '\n';
     }
 
     // Allocate chars
-    if (auto* p = simple_arena.create<char>(1))
-    {
+    if (auto *p = simple_arena.create<char>(1)) {
         *p = 'a';
-        std::cout << "allocated a char at " << (void*)p << '\n';
+        std::cout << "allocated a char at " << (void *) p << '\n';
     }
 
     return 0;
