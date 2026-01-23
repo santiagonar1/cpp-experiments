5d25134f3ddfa3e88a6cb47f5dd9302b3386b725
Santiago Narvaez <santiagonar1@gmail.com>
2026-01-23 15:29:21 +0100

    feat: first dummy example using threads

--- clocks/apps/main.cpp
+++ clocks/apps/main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <mutex>
+#include <thread>
+#include <vector>
+
+std::mutex mutex;
+void say_hi(const unsigned int id) {
+    auto lock = std::unique_lock{mutex};
+    std::cout << "Hello from thread: " << id << std::endl;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    auto threads = std::vector<std::thread>{};
+
+    for (unsigned int i = 0; i < 10; ++i) { threads.emplace_back(say_hi, i); }
+
+    for (auto &thread: threads) { thread.join(); }
+
     return 0;
 }
